a266ca39bfc17f11b24cc7b18c62cb6f0b4f8c57
Aslak Knutsen <aslak@4fs.no>
2017-02-09 12:17:12 +0100

    Add centos-ci node setup

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+/usr/sbin/setenforce 0
+
+# Get all the deps in
+yum -y install \
+    docker \
+    make \
+    git \
+    curl
+
+sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --log-driver=journald --insecure-registry registry.ci.centos.org:5000"' /etc/sysconfig/docker
+service docker start
+
+
 make docker-build-build
 make docker-install
 make docker-test
